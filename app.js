98cb58b63ff772dc356b776d6fcf245b28d326bf
Lucas <75747405+Lucas-02@users.noreply.github.com>
2022-01-14 07:00:09 -0800

    Created SAC prototype

--- app.js
+++
@@ -1,9 +0,0 @@
-anime ({
-    targets: '#lineDrawing .lines path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 50000,
-    delay: function (el, i) { return i * 250 },
-    direction: 'alternate',
-    loop: false
-});
---
+++ main.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("form");
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const selected = event.target.querySelector("input[type=submit]:focus");
+    console.log(selected);
+    if (selected.classList.contains("yes"))
+        document.location.href = "/yes-page.html";
+    else if (selected.classList.contains("no"))
+        document.location.href = "https://youtu.be/dQw4w9WgXcQ";
+}
